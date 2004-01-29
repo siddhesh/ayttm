d0143295a6c9074d5816d49072c15cecbd6e08b0
bluesmoon <bluesmoon>
2004-01-29 05:13:13 +0000

    merge in from libyahoo2

--- modules/yahoo2/libyahoo2/libyahoo2.c
+++ modules/yahoo2/libyahoo2/libyahoo2.c
@@ -813,9 +813,9 @@ static void yahoo_input_close(struct yahoo_input_data *yid)
 	inputs = y_list_remove(inputs, yid);
 
 	LOG(("yahoo_input_close(read)")); 
-	YAHOO_CALLBACK(ext_yahoo_remove_handler)(yid->read_tag);
+	YAHOO_CALLBACK(ext_yahoo_remove_handler)(yid->yd->client_id, yid->read_tag);
 	LOG(("yahoo_input_close(write)")); 
-	YAHOO_CALLBACK(ext_yahoo_remove_handler)(yid->write_tag);
+	YAHOO_CALLBACK(ext_yahoo_remove_handler)(yid->yd->client_id, yid->write_tag);
 	yid->read_tag = yid->write_tag = 0;
 	if(yid->fd)
 		close(yid->fd);
@@ -1768,9 +1768,10 @@ static void yahoo_process_auth_0x0b(struct yahoo_input_data *yid, const char *se
 
 		/* Bad.  Abort.
 		 */
-		if ((magic_cnt + 1 > magic_len) || 
-				(magic_cnt > magic_len))
+		if (magic_cnt >= magic_len) {
+			WARNING(("magic_cnt(%d)  magic_len(%d)", magic_cnt, magic_len))
 			break;
+		}
 
 		byte1 = magic[magic_cnt];
 		byte2 = magic[magic_cnt+1];
@@ -2972,7 +2973,7 @@ int yahoo_write_ready(int id, int fd, void *data)
 			y_list_free_1(l);
 		}
 		LOG(("yahoo_write_ready(%d, %d) len < 0", id, fd));
-		YAHOO_CALLBACK(ext_yahoo_remove_handler)(yid->write_tag);
+		YAHOO_CALLBACK(ext_yahoo_remove_handler)(id, yid->write_tag);
 		yid->write_tag = 0;
 		errno=e;
 		return 0;
@@ -2996,7 +2997,7 @@ int yahoo_write_ready(int id, int fd, void *data)
 		*/
 		if(!yid->txqueues) {
 			LOG(("yahoo_write_ready(%d, %d) !yxqueues", id, fd));
-			YAHOO_CALLBACK(ext_yahoo_remove_handler)(yid->write_tag);
+			YAHOO_CALLBACK(ext_yahoo_remove_handler)(id, yid->write_tag);
 			yid->write_tag = 0;
 		}
 	}
--- modules/yahoo2/yahoo.c
+++ modules/yahoo2/yahoo.c
@@ -136,8 +136,8 @@ PLUGIN_INFO plugin_info =
 	PLUGIN_SERVICE,
 	"Yahoo",
 	"Provides Yahoo Instant Messenger support",
-	"$Revision: 1.89 $",
-	"$Date: 2004/01/07 05:28:44 $",
+	"$Revision: 1.90 $",
+	"$Date: 2004/01/29 05:13:14 $",
 	&ref_count,
 	plugin_init,
 	plugin_finish,
@@ -3215,7 +3215,7 @@ static int ext_yahoo_add_handler(int id, int fd, yahoo_input_condition cond, voi
 	return d->tag;
 }
 
-static void ext_yahoo_remove_handler(int tag)
+static void ext_yahoo_remove_handler(int id, int tag)
 {
 	YList * l;
 	for(l = handlers; l; l = l->next)
